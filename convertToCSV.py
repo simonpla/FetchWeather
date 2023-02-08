3d59b74d800da175330363d82d2936cefc425c42
Simon Plt <kamelisum.robocup@gmail.com>
2023-02-08 12:32:05 +0100

    weather downloadable by location and time period, insertable into csv file

---
+++ convertToCSV.py
@@ -0,0 +1,85 @@
+import argparse
+import csv
+import glob
+import json
+
+parser = argparse.ArgumentParser(
+    prog='convertToCSV',
+    description='Reads data from JSON and appends relevant data from it into a CSV file',
+    epilog='Text at the bottom of help')
+
+parser.add_argument('-d', '--directory', default="out", dest='in_dir')
+parser.add_argument('-o', '--out', default="training.csv", type=str)
+
+params_header = ['location', 'date', 'temp', 'wind', 'gust', 'clouds', 'weatherId', 'weatherGroup', 'rain', 'snow']
+
+
+def list_weather_data_files():
+    """
+    Reads all json file names from given dir into list
+    :return:
+    """
+    weather_data = []
+    for json_file_weather in glob.glob(f'{args.in_dir}/*.json'):
+        weather_data.append(json_file_weather)
+    return weather_data
+
+
+def load_weather_json(file):
+    """
+    Reads json file and loads it using json lib
+    :param file: file location
+    :return:
+    """
+    f = open(file, 'r')
+    contents = f.read()
+    f.close()
+    return json.loads(contents)['list']
+
+
+def get_params(hour_data, ):
+    """
+    Filters out the relevant data, optionally assigns -1 to missing parameters
+    :param hour_data: which hour
+    :return:
+    """
+    # optionally missing params
+    if 'rain' in hour_data.keys():
+        rain = hour_data['rain']['1h']
+    else:
+        rain = -1
+
+    if 'snow' in hour_data.keys():
+        snow = hour_data['snow']['1h']
+    else:
+        snow = -1
+
+    return [
+        weather_data_str.split('-')[1].replace(',', ' ') or -1,  # location name, replace comma with space
+        hour_data['dt'],  # date in unix format
+        hour_data['main']['temp'],  # temperature
+        hour_data['wind']['speed'],  # wind speed
+        hour_data['wind']['gust'],  # gust speed
+        hour_data['clouds']['all'],  # cloudiness
+        hour_data['weather'][0]['id'],  # weather condition id
+        hour_data['weather'][0]['main'],  # weather description
+        rain,  # rain in the last hour
+        snow  # snow in the last hour
+    ]
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+
+    with open(args.out, 'w', encoding='UTF8', newline='') as weather_dest:
+        writer = csv.writer(weather_dest)
+
+        writer.writerow(params_header)  # write header
+
+        for weather_data_str in list_weather_data_files():
+            weather_data_json = load_weather_json(weather_data_str)  # load contents from json file
+
+            for hour in range(24):
+                params = get_params(weather_data_json[hour])  # get relevant data from json
+                writer.writerow(params)  # write data for every hour of the day
---
+++ getWeatherPeriod.py
@@ -0,0 +1,133 @@
+import argparse
+import json
+import os
+import urllib.parse
+import urllib.request
+from datetime import datetime, timedelta
+
+D__M__Y = "%d.%m.%Y"
+
+parser = argparse.ArgumentParser(
+    prog='fetchWeatherByDate',
+    description='Fetches Weather by Date into a json file from OWM',
+    epilog='Text at the bottom of help')
+
+parser.add_argument('-k', '--key', default=os.getenv('API_OWM'), dest='api_key')
+parser.add_argument('-d', '--directory', default="out", dest='out_dir')
+parser.add_argument('-s', '--start', default="01.10.2022", type=str)
+parser.add_argument('-e', '--end', default="05.10.2022", type=str)
+parser.add_argument('-n', '--name', default="Odense,DK", type=str)
+parser.add_argument('-i', '--index', default="0", type=str)
+
+
+def parse_date(date_string):
+    """
+    Parses date string to date object
+    :param date_string: format "%d.%m.%Y"
+    :return: date object
+    """
+    return datetime.strptime(date_string, D__M__Y)
+
+
+def to_interval(start_date, end_date):
+    """
+    Convert date range to closed interval
+    :param start_date:
+    :param end_date:
+    :return:
+    """
+    if start_date <= end_date:
+        return [start_date + timedelta(days=x) for x in range(0, (end_date - start_date).days + 1)]
+    return []
+
+
+def create_dir(dir_path):
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+def to_unix(python_date):
+    """
+    Convert python date to unix time stamp
+    :param python_date:
+    :return:
+    """
+    return int(python_date.timestamp())
+
+
+def to_date_string(python_date):
+    """
+    Convert python date to human-readable date
+    :param python_date:
+    :return:
+    """
+    return python_date.strftime('%Y.%m.%d')
+
+
+def fetch_historical_data(key, start, end, loc):
+    """
+    Download json data for single day
+    :param loc: location with lat and lon as tuple
+    :param end: end date in unix format
+    :param start: start date in unix format
+    :param key: api key
+    :return:
+    """
+    base_url = 'https://history.openweathermap.org/data/2.5/history/city?'
+    arguments = dict(appid=key, units='metric', lat=loc[0], lon=loc[1], type='hour', start=start, end=end)
+    request_url = base_url + urllib.parse.urlencode(arguments)
+
+    with urllib.request.urlopen(request_url) as request:
+        return request.read().decode("utf8")
+
+
+def get_location_by_name(key, query, index):
+    """
+    Get location by given name
+    :param index: which city from the list if names match
+    :param key:
+    :param query:
+    :return: tuple with lat, lon
+    """
+    base_url = 'http://api.openweathermap.org/geo/1.0/direct?'
+    arguments = dict(q=query, appid=key, limit='5')
+    request_url = base_url + urllib.parse.urlencode(arguments)
+
+    with urllib.request.urlopen(request_url) as request:
+        # get data, load into json and use city from index
+        city_loc = json.loads(request.read().decode("utf8"))[int(index)]
+        return city_loc['lat'], city_loc['lon']
+
+
+def exists(file):
+    """
+    checks whether file exists
+    :param file:
+    :return:
+    """
+    return os.path.exists(file) and os.path.isfile(file)
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)
+
+    create_dir(args.out_dir)
+    
+    location = get_location_by_name(args.api_key, args.name, args.index)
+
+    dates = to_interval(parse_date(args.start), parse_date(args.end))
+    for date in dates:
+        file_name = f'{args.out_dir}/{to_date_string(date)}-{args.name}-hist.json'
+
+        if exists(file_name):
+            print(f'Already downloaded: {to_date_string(date)}')
+            continue
+
+        print(f"Downloading: {to_date_string(date)} ...")
+
+        end_of_day = datetime(date.year, date.month, date.day, 23, 59, 59)
+        json_str = fetch_historical_data(args.api_key, to_unix(date), to_unix(end_of_day), location)
+
+        f = open(file_name, 'w')
+        f.write(json_str)
