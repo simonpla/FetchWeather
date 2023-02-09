47b3dd6c273fdb28ceb31fe557f652f50b01b4a0
simonpla <63154196+simonpla@users.noreply.github.com>
2023-02-09 12:20:12 +0100

    different calculation of yesterday
    for alpine linux

--- daily_weather_fetch.sh
+++ daily_weather_fetch.sh
@@ -3,7 +3,7 @@
 python=/usr/bin/python3
 
 locations=("Odense,DK" "Svendborg,DK")
-yesterday=$(date --date="yesterday" +"%d.%m.%Y")
+yesterday=$(date -d "@$(($(date +%s) - 86400))" +"%d.%m.%Y")
 
 for location in ${locations[@]}; do
 	$python ~/FetchWeather/getWeatherPeriod.py -d out -s 01.10.2022 -e $yesterday -n $location -k $OWM_KEY
