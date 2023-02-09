0ae9dca1bf579635422893208f957eb1d43db41c
simonpla <63154196+simonpla@users.noreply.github.com>
2023-02-09 14:09:11 +0100

    improved location use

--- daily_weather_fetch.sh
+++ daily_weather_fetch.sh
@@ -6,5 +6,5 @@ locations=("Odense,DK" "Svendborg,DK")
 yesterday=$(date -d "@$(($(date +%s) - 86400))" +"%d.%m.%Y")
 
 for location in ${locations[@]}; do
-	$python ~/FetchWeather/getWeatherPeriod.py -d out -s 01.10.2022 -e $yesterday -n $location -k $OWM_KEY
+	$python ~/FetchWeather/getWeatherPeriod.py -d ~/FetchWeather/out -s 01.10.2022 -e $yesterday -n $location -k $OWM_KEY
 done
