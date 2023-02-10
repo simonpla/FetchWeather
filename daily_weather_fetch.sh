d05a341270fba9af12a5a2c70f8376f0a7fdf83c
Simon Plt <kamelisum.robocup@gmail.com>
2023-02-10 09:40:22 +0100

    small changes in scripts for data handling

--- daily_weather_fetch.sh
+++ daily_weather_fetch.sh
@@ -5,6 +5,12 @@ python=/usr/bin/python3
 locations=("Odense,DK" "Svendborg,DK")
 yesterday=$(date -d "@$(($(date +%s) - 86400))" +"%d.%m.%Y")
 
+echo "`date`"
+
 for location in ${locations[@]}; do
-	$python ~/FetchWeather/getWeatherPeriod.py -d ~/FetchWeather/out -s 01.10.2022 -e $yesterday -n $location -k $OWM_KEY
+	$python ~/FetchWeather/getWeatherPeriod.py -d ~/FetchWeather/out -s 10.02.2022 -e $yesterday -n $location -k $OWM_KEY
 done
+
+# Pack everything into one CSV file
+$python ~/FetchWeather/convertToCSV.py -d ~/FetchWeather/out -o ~/FetchWeather/out/weather_dk.csv -t=history
+
--- future_weather_fetch.sh
+++ future_weather_fetch.sh
@@ -2,8 +2,18 @@
 
 python=/usr/bin/python3
 
-locations=("Odense,DK" "Svendborg,DK")
+# delete old predictions
+rm -f ~/FetchWeather/out/*fut*
 
+echo "`date`"
+
+locations=("Odense,DK" "Svendborg,DK")
 for location in ${locations[@]}; do
 	$python ~/FetchWeather/getWeatherPeriod.py -n $location --key $OWM_KEY -t future -d ~/FetchWeather/out
 done
+
+# Pack everything into one CSV file
+$python ~/FetchWeather/convertToCSV.py -d ~/FetchWeather/out -o ~/FetchWeather/out/weather_dk_predictions.csv -t=future
+
+# Copy to archive
+cp ~/FetchWeather/out/weather_dk_predictions.csv ~/FetchWeather/out/weather_dk_predictions_`date +"%Y%m%d_%H%M%S"`.csv
